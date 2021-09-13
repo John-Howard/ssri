c386f1eb2143a600b07a145106bbff463486a330
John Howard <john.howard@hertfordshire.gov.uk>
2021-09-13 13:53:32 +0100

    markers linked document url

--- markers/models.py
+++ markers/models.py
@@ -9,6 +9,7 @@ class Marker(models.Model):
     name = models.CharField(max_length=255)
     location = models.PointField()
     description = models.CharField(max_length=255, default='')
+    linked_document = models.URLField(max_length=200, default='')
 
     def __str__(self):
         """Return string representation."""
