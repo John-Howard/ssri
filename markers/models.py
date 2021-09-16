25bce5931766306f2df970989d2493d3146f3aec
John Howard <1853765+John-Howard@users.noreply.github.com>
2021-09-16 10:38:39 +0100

    markers fields added

--- markers/models.py
+++ markers/models.py
@@ -8,6 +8,10 @@ class Marker(models.Model):
     """A marker with name and location."""
 
     name = models.CharField(max_length=255)
+    reference_code = models.CharField(max_length=50, default='')
+    classification_or_type = models.CharField(max_length=255, default='')
+    is_published = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now=True)
     location = models.PointField()
     description = models.CharField(max_length=255, default='')
     linked_document = models.URLField(max_length=200, default='')
