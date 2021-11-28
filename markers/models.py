f77238ce340cfde0c33029b827e59a6b8487e99a
John Howard <1853765+John-Howard@users.noreply.github.com>
2021-11-28 10:02:55 +0000

    formatting changes - black

--- markers/models.py
+++ markers/models.py
@@ -7,16 +7,16 @@ from organisations.models import Organisation
 class Marker(models.Model):
     """A marker with name and location."""
 
-    name = models.CharField('Risk Title', max_length=255)
-    reference_code = models.CharField(max_length=50, default='')
-    classification_or_type = models.CharField(max_length=255, default='')
+    name = models.CharField("Risk Title", max_length=255)
+    reference_code = models.CharField(max_length=50, default="")
+    classification_or_type = models.CharField(max_length=255, default="")
     is_published = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     location = models.PointField()
-    description = models.CharField(max_length=255, default='')
-    linked_document = models.URLField(max_length=200, default='')
-    uprn = models.PositiveBigIntegerField('UPRN', default=0)
+    description = models.CharField(max_length=255, default="")
+    linked_document = models.URLField(max_length=200, default="")
+    uprn = models.PositiveBigIntegerField("UPRN", default=0)
     organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
