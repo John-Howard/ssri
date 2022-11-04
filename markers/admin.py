d3e8958f2558cc3012bbf09f29ebeeaa423908e0
John Howard <1853765+John-Howard@users.noreply.github.com>
2022-11-04 15:44:36 +0000

    subclass GISModelAdmin to pick up options

--- markers/admin.py
+++ markers/admin.py
@@ -1,12 +1,34 @@
 """Markers admin."""
 
 from django.contrib.gis import admin
-
 from markers.models import Marker
 
 
+class CustomGeoModelAdmin(admin.GISModelAdmin):
+    gis_widget_kwargs = {
+        "attrs": {
+            "default_zoom": 9,
+            "default_lon": -0.2,
+            "default_lat": 51.9,
+            "map_width": 700,
+            "map_height": 500,
+        },
+    }
+
+
 @admin.register(Marker)
-class MarkerAdmin(admin.OSMGeoAdmin):
+class MarkerAdmin(CustomGeoModelAdmin):
+    pass
     """Marker admin."""
 
-    list_display = ("name", "is_published", "reference_code", "location", "description", "linked_document", "classification_or_type", "created_at", "last_updated")
+    list_display = (
+        "name",
+        "is_published",
+        "reference_code",
+        "location",
+        "description",
+        "linked_document",
+        "classification_or_type",
+        "created_at",
+        "last_updated",
+    )
