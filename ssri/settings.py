399888aef13e0fb9c2016c63636f257a9da9d112
John Howard <1853765+John-Howard@users.noreply.github.com>
2022-11-04 11:43:37 +0000

    updated ALLOWED_HOSTS for .localhost

--- ssri/settings.py
+++ ssri/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ssri.localdev.me"]
+ALLOWED_HOSTS = [".localdev.me", ".localhost", "127.0.0.1", "[::1]"]
 
 
 # Application definition
