bb4130ef9a43419e71236aff0c78e5cf3a317f07
John Howard <1853765+John-Howard@users.noreply.github.com>
2022-11-07 20:45:48 +0000

    re-order UPRN

--- organisations/models.py
+++ organisations/models.py
@@ -6,13 +6,13 @@ from django.contrib.gis.db import models
 class Organisation(models.Model):
     """An organisation with name and location."""
 
+    uprn = models.PositiveBigIntegerField("UPRN", default=0)
     name = models.CharField(max_length=255)
     building_name_or_number = models.CharField(max_length=255)
     street = models.CharField(max_length=255)
     city_or_town = models.CharField(max_length=255)
     postcode = models.CharField(max_length=8)
     location = models.PointField()
-    uprn = models.PositiveBigIntegerField(default=0)
 
     def __str__(self):
         """Return string representation."""
