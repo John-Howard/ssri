cddd45bec19c8b94896cb94f7fb04514aa7bfcb7
John Howard <1853765+John-Howard@users.noreply.github.com>
2022-11-08 22:18:42 +0000

    fix urls after file rename

--- ssri/urls.py
+++ ssri/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('markers.api_urls')),
-    path('markers/', include('markers.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("markers.api")),
+    path("markers/", include("markers.urls")),
 ]
